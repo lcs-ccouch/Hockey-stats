af3e2ecf63cf1ed236812ec9c22670d55e334657
Calum Couch <ccouch@lcs.on.ca>
2023-05-31 12:12:15 -0400

    made a list view with examples for a favourite player tracker

--- Hockey stats/FavouritePlayers.swift
+++ Hockey stats/FavouritePlayers.swift
@@ -7,9 +7,37 @@
 
 import SwiftUI
 
+struct Player: Identifiable {
+    let id = UUID()
+    let playerName: String
+    let goals: Int
+    let assists: Int
+    let gamesPlayed: Int
+}
+
 struct FavouritePlayers: View {
+    let players = [
+        Player(playerName: "Connor McDavid", goals: 31, assists: 14, gamesPlayed: 28),
+        Player(playerName: "Sidney Crosby", goals: 26, assists: 8, gamesPlayed: 30),
+        Player(playerName: "Alex Ovechkin", goals: 18, assists: 9, gamesPlayed: 22),
+        Player(playerName: "Victor Headman", goals: 23, assists: 10, gamesPlayed: 25),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(players) { player in
+                VStack(alignment: .leading) {
+                    Text(player.playerName)
+                        .font(.headline)
+                    HStack {
+                        Text("Goals: \(player.goals)")
+                        Text("Assists: \(player.assists)")
+                        Text("Games Played: \(player.gamesPlayed)")
+                    }
+                }
+            }
+            .navigationTitle("Favourite Players")
+        }
     }
 }
 
