797ff98c6179c4d77af633a889c6b26a157facc2
Calum Couch <ccouch@lcs.on.ca>
2023-05-31 12:04:16 -0400

    Initial Commit

---
+++ Hockey stats/Hockey_statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hockey_statsApp.swift
+//  Hockey stats
+//
+//  Created by calum couch on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Hockey_statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
